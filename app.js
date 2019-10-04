b79f39982c2145b42cde97cd1ee6cc7c99a6f74f
unicorn-84 <unicorn-84@ya.ru>
2019-10-05 00:40:22 +0300

    Change app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-app.get('/', async (req, res) => res.status(200).send('Express'));
+app.get('/', async (req, res) => res.status(200).send('Deployment-testing'));
 app.listen(process.env.APP_PORT || 9000, process.env.APP_HOST || '127.0.0.1');
